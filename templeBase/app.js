879eacf00399d9d8ce0d689b607a3546e064336c
eduardlio <liao.edward@gmail.com>
2021-01-12 08:23:50 +1000

    Move rendering to zodiac-specific level

--- templeBase/app.js
+++ templeBase/app.js
@@ -6,19 +6,8 @@ let state = {};
 let wrap;
 let activeSignUI;
 
-const rerenderActiveSigns = () => {
-    if (!activeSignUI) {
-        activeSignUI = document.getElementById("activeSign");
-    }
-    activeSignUI.innerText = Object.values(state)
-        .filter((sign) => sign.isActive)
-        .map((activeSign) => activeSign.sign)
-        .join(", ");
-};
-
 const toggleSignActive = (sign) => {
     state[sign] = state[sign].toggleActive();
-    rerenderActiveSigns();
 };
 ipcRenderer.on("registered-zodiac-click", (event, arg) => {
     if (arg.success) {
@@ -30,11 +19,23 @@ ipcRenderer.on("registered-zodiac-click", (event, arg) => {
 
 const loadZodiacButtons = () => {
     wrap = document.getElementById("zodiac-button-wrap");
+    activeSign = document.getElementById('activeSign')
     const buttons = signs.map((sign) => {
-        state[sign] = new ZodiacSign(sign);
+        // create activeSigns list
+        const listElement = document.createElement('li')
+        listElement.id = `ui-node-${sign}`
+        listElement.innerText = sign
+        listElement.classList.add('ui-node')
+        activeSign.appendChild(listElement)
+        state[sign] = new ZodiacSign(sign, listElement);
+        // create buttons
         wrap.appendChild(state[sign].node);
         return state[sign].node;
     });
     return buttons;
 };
+function switchOffAll() {
+    Object.values(state).forEach(horoscope => {
+    })
+}
 loadZodiacButtons();
