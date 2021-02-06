aeb9a3aa350b5d31f230bb974ea576016c2b6ba2
Edward Liao <edward.liao@blackbook.ai>
2021-02-06 21:56:20 +1000

    Added proper types

--- vue-temple/src/types/index.ts
+++ vue-temple/src/types/index.ts
@@ -2,61 +2,61 @@ export interface Zodiac {
 	name: string;
 }
 export type ColorOption =
-	| 'LED_ORANGE1'
-	| 'LED_ORANGE2'
-	| 'LED_ORANGE3'
-	| 'LED_YELLOW'
-	| 'LED_GREEN1'
-	| 'LED_GREEN2'
-	| 'LED_BLUE1'
-	| 'LED_BLUE2'
-	| 'LED_BLUE3'
-	| 'LED_BLUE4'
-	| 'LED_PURPLE1'
-	| 'LED_PURPLE2'
-	| 'LED_PINK'
-	| 'LED_WHITE'
-	| 'LED_RED'
+	| 'ORANGE1'
+	| 'ORANGE2'
+	| 'ORANGE3'
+	| 'YELLOW'
+	| 'GREEN1'
+	| 'GREEN2'
+	| 'BLUE1'
+	| 'BLUE2'
+	| 'BLUE3'
+	| 'BLUE4'
+	| 'PURPLE1'
+	| 'PURPLE2'
+	| 'PINK'
+	| 'WHITE'
+	| 'RED'
 	export interface ColorSelectionOption {
 		option: ColorOption;
 		bg: string
 	}
 export const colorSelectionOptions: ColorSelectionOption[] = [
-	{option: 'LED_ORANGE1', bg: 'bg-yellow-600'},
-	{option: 'LED_ORANGE2', bg: 'bg-yellow-500'},
-	{option: 'LED_ORANGE3', bg: 'bg-yellow-400'},
-	{option: 'LED_YELLOW', bg: 'bg-yellow-300'},
-	{option: 'LED_GREEN1', bg: 'bg-green-600'},
-	{option: 'LED_GREEN2', bg: 'bg-green-400'},
-	{option: 'LED_BLUE1', bg: 'bg-blue-800'},
-	{option: 'LED_BLUE2', bg: 'bg-blue-700'},
-	{option: 'LED_BLUE3', bg: 'bg-blue-600'},
-	{option: 'LED_BLUE4', bg: 'bg-blue-500'},
-	{option: 'LED_PURPLE1', bg: 'bg-purple-500'},
-	{option: 'LED_PURPLE2', bg: 'bg-purple-400'},
-	{option: 'LED_PINK', bg: 'bg-pink-600'},
-	{option: 'LED_WHITE', bg: 'bg-gray-50'},
-	{option: 'LED_RED', bg: 'bg-red-600'},
+	{option: 'ORANGE1', bg: 'bg-yellow-600'},
+	{option: 'ORANGE2', bg: 'bg-yellow-500'},
+	{option: 'ORANGE3', bg: 'bg-yellow-400'},
+	{option: 'YELLOW', bg: 'bg-yellow-300'},
+	{option: 'GREEN1', bg: 'bg-green-600'},
+	{option: 'GREEN2', bg: 'bg-green-400'},
+	{option: 'BLUE1', bg: 'bg-blue-800'},
+	{option: 'BLUE2', bg: 'bg-blue-700'},
+	{option: 'BLUE3', bg: 'bg-blue-600'},
+	{option: 'BLUE4', bg: 'bg-blue-500'},
+	{option: 'PURPLE1', bg: 'bg-purple-500'},
+	{option: 'PURPLE2', bg: 'bg-purple-400'},
+	{option: 'PINK', bg: 'bg-pink-600'},
+	{option: 'WHITE', bg: 'bg-gray-50'},
+	{option: 'RED', bg: 'bg-red-600'},
 ]
 
 export type LightMode = ''
-	| 'LED_ON'
-	| 'LED_OFF'
-	| 'LED_SPEEDUP'
-	| 'LED_SPEEDDOWN'
-	| 'LED_FLASH'
-	| 'LED_STROBE'
-	| 'LED_FADE'
-	| 'LED_SMOOTH'
+	| 'ON'
+	| 'OFF'
+	| 'SPEEDUP'
+	| 'SPEEDDOWN'
+	| 'FLASH'
+	| 'STROBE'
+	| 'FADE'
+	| 'SMOOTH'
 export const ligthModes: LightMode[] = [
-	'LED_ON',
-	'LED_OFF',
-	'LED_SPEEDUP',
-	'LED_SPEEDDOWN',
-	'LED_FLASH',
-	'LED_STROBE',
-	'LED_FADE',
-	'LED_SMOOTH',
+	'ON',
+	'OFF',
+	'SPEEDUP',
+	'SPEEDDOWN',
+	'FLASH',
+	'STROBE',
+	'FADE',
+	'SMOOTH',
 ]
 
 export const zodiacs: Zodiac[] = [
