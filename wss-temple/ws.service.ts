cf89f17788d032861593817289fb26d1c30a7c16
Edward Liao <edward.liao@blackbook.ai>
2021-02-08 19:10:42 +1000

    Add functionality for client emit

--- wss-temple/ws.service.ts
+++ wss-temple/ws.service.ts
@@ -2,6 +2,7 @@ import * as WebSocket from 'ws'
 class SocketHelper {
 	port?: number
 	server?: WebSocket.Server
+	clients: Set<WebSocket> = new Set()
 	constructor(port: number) {
 		this.port = port;
 		this.ensureInit()
@@ -13,15 +14,20 @@ class SocketHelper {
 			})
 		}
 		this.server.on('connection', (socket: WebSocket) => {
+			this.clients.add(socket)
 			socket.on('message', (data: WebSocket.Data) => {
+			})
+			socket.on('close', () => {
+				this.clients.delete(socket)
 			})
 		})
 		return this.server
 	}
 	sendZodiacCommand(zodiac: string, command: string) {
 		const emitEvent = `${zodiac}-${command}`;
-		const server = this.ensureInit()
-		server.emit(emitEvent)
+		this.clients.forEach(socket => {
+			socket.send(emitEvent)
+		})
 	}
 }
 const PORT = +(process.env.WSS_PORT || 8000)
