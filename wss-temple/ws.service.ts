82bc6132587e543a3c63cb244643a643e0caab1d
Edward Liao <edward.liao@blackbook.ai>
2021-02-08 19:18:40 +1000

    Add api route to allow for mode/color difference

--- wss-temple/ws.service.ts
+++ wss-temple/ws.service.ts
@@ -8,23 +8,21 @@ class SocketHelper {
 		this.ensureInit()
 	}
 	private ensureInit(): WebSocket.Server {
-		if (!this.server) {
+		if(!this.server) {
 			this.server = new WebSocket.Server({
-				port: this!.port
+				port: this!.port,
 			})
 		}
 		this.server.on('connection', (socket: WebSocket) => {
 			this.clients.add(socket)
-			socket.on('message', (data: WebSocket.Data) => {
-			})
 			socket.on('close', () => {
 				this.clients.delete(socket)
 			})
 		})
 		return this.server
 	}
-	sendZodiacCommand(zodiac: string, command: string) {
-		const emitEvent = `${zodiac}-${command}`;
+	sendZodiacCommand(zodiac: string, command: string, value: string) {
+		const emitEvent = `${zodiac}-${command}-${value}`;
 		this.clients.forEach(socket => {
 			socket.send(emitEvent)
 		})
