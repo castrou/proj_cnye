14ca02d0bdfdea0ebb00e7e517ef617fcf9e0b88
eduardlio <liao.edward@gmail.com>
2021-02-11 15:32:35 +1000

    Better layout

--- vue-temple/src/App.vue
+++ vue-temple/src/App.vue
@@ -1,18 +1,20 @@
 <template lang="pug">
-.bg-gray-800.p-10.min-h-screen.flex
-  .container.mx-auto(class="min-w-3/6")
-    Stepper
-  .container
-    .flex.space-x-2.mx-auto(class="min-w-3/6")
-      button.rounded-sm.bg-gray-50.flex-1(@click.prevent="allOn") All On
-      button.rounded-sm.bg-gray-50.flex-1(@click.prevent="allOff") All Off
-      button.rounded-sm.bg-gray-50.flex-1(@click.prevent="allFade") All Fade
-    .flex.flex-col.space-y-2.mx-auto(class="min-w-3/6")
-      ZodiacLight(
-      v-for="(zodiac, index) in zodiacs" 
-      :zodiac="zodiac"
-      :key="zodiac + index"
-  )
+.bg-gray-800.p-10.min-h-screen.w-vw.mx-auto
+  h1.text-gray-50 YAD LED SHOW
+  .flex
+    .container.mx-auto(class="min-w-3/6")
+      .flex.space-x-2.mx-auto.mb-2.p-4(class="min-w-3/6")
+        button.rounded-sm.bg-gray-50.flex-1.p-4(@click.prevent="allOn") All On
+        button.rounded-sm.bg-gray-50.flex-1(@click.prevent="allOff") All Off
+        button.rounded-sm.bg-gray-50.flex-1(@click.prevent="allFade") All Fade
+      Stepper
+    .container
+      .flex.flex-col.space-y-2.mx-auto(class="min-w-3/6")
+        ZodiacLight(
+        v-for="(zodiac, index) in zodiacs" 
+        :zodiac="zodiac"
+        :key="zodiac + index"
+    )
 </template>
 
 <script lang="ts">
